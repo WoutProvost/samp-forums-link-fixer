635edc5cb3b822d169205f0a00458d41a4ec4db6
Freaksken <wout_provost@hotmail.com>
2018-09-28 21:14:48 +0200

    Add support for youtu.be

--- js/content.js
+++ js/content.js
@@ -4,9 +4,9 @@ for(var i = 0; i < elements.length; i++) {
 	var href_original = elements[i].getAttribute("href");
 	var text_original = elements[i].text;
 
-	if(href_original && href_original.match(/(http(s)?:\/\/)?(www\.)?\*{7}\.[a-z]{2,}\/watch\?v=.*/)) {
-		var href_replaced = href_original.replace(/\*{7}/, "youtube");
-		var text_replaced = text_original.replace(/\*{7}/, "youtube");
+	if(href_original && href_original.match(/(http(s)?:\/\/)?(www\.)?((\*{7}\.[a-z]{2,})|(\*{6}\.be))\/watch\?v=.*/)) {
+		var href_replaced = href_original.replace(/\*{6,7}/, "youtube");
+		var text_replaced = text_original.replace(/\*{6,7}/, "youtube");
 
 		if(href_original !== href_replaced) {
 			elements[i].setAttribute("href", href_replaced);
