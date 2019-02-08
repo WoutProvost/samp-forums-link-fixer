9d22d9173e7683d55a125a3e204c7598ad30fe7d
Jamie Taylor <jamie@dr0id.in>
2019-02-08 18:20:19 +0000

    Create userscript file to call in the actual code
    All we really need are the headers and then the extension will sort the rest for us. Happy days!

---
+++ sa-mp-links.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          SA-MP Forums link fixer
+// @namespace     https://github.com/WoutProvost/samp-forums-link-fixer
+// @version       0.1
+// @description   Userscript to fix censored YouTube links on the SA-MP Forums
+// @author        Freaksken - Script creation (https://github.com/WoutProvost)
+// @author        Jamie - Userscript "conversion" (https://github.com/jtylr)
+// @match         https://forum.sa-mp.com/*
+// @require       https://raw.githubusercontent.com/WoutProvost/samp-forums-link-fixer/master/js/content.js
+// @updateURL     https://raw.githubusercontent.com/WoutProvost/samp-forums-link-fixer/master/sa-mp-links.user.js
+// @grant         none
+// ==/UserScript==
+
+(function() {
+  /*
+    We don't even need anything here, all we need are the headers
+    above to load in the latest version of the script (from master)
+  */
+})();
